b62d1077b3f9380858b99a0b0a6e519780c0b794
GRJeon <104538652+GRJeon@users.noreply.github.com>
2026-04-29 21:50:48 +0900

    feat: movie entity 생성

---
+++ MovieExplorer/MovieExplorer/Domain/Entities/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  MovieExplorer
+//
+//  Created by Liam on 4/29/26.
+//
+
+struct Movie: Equatable {
+    let id: Int
+    let title: String
+    let posterPath: String?
+    let voteAverage: Double
+}
