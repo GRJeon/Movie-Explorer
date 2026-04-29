64f330a0c80d673c55e5a2eda9ba137839eedf25
GRJeon <104538652+GRJeon@users.noreply.github.com>
2026-04-30 08:42:08 +0900

    feat: APIEndpoint protocol 생성

---
+++ MovieExplorer/MovieExplorer/Data/Network/APIEndpoint.swift
@@ -0,0 +1,21 @@
+//
+//  APIEndpoint.swift
+//  MovieExplorer
+//
+//  Created by Liam on 4/30/26.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+protocol APIEndpoint {
+    var baseURL: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var headers: [String: String]? { get }
+    var parameters: [String: Any]? { get }
+}
