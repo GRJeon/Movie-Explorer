4c82d2c5f9938185475242c5dd3ba925811448f6
GRJeon <104538652+GRJeon@users.noreply.github.com>
2026-04-30 12:22:15 +0900

    refactor: repository의 에러는 MovieError 타입으로 throw

--- MovieExplorer/MovieExplorer/Data/Repositories/DefaultMovieRepository.swift
+++ MovieExplorer/MovieExplorer/Data/Repositories/DefaultMovieRepository.swift
@@ -16,22 +16,48 @@ final class DefaultMovieRepository: MovieRepositoryProtocol {
     }
 
     func fetchPopularMovies(page: Int) async throws -> (movies: [Movie], totalPages: Int) {
-        let endpoint = MovieEndpoint.popular(page: page)
-        let response: MovieResponseDTO = try await networkService.request(endpoint: endpoint)
-        let movies = response.results.map { $0.toDomain() }
-        return (movies: movies, totalPages: response.totalPages)
+        do {
+            let endpoint = MovieEndpoint.popular(page: page)
+            let response: MovieResponseDTO = try await networkService.request(endpoint: endpoint)
+            let movies = response.results.map { $0.toDomain() }
+            return (movies: movies, totalPages: response.totalPages)
+        } catch {
+            throw mapError(error)
+        }
     }
 
     func searchMovies(query: String, page: Int) async throws -> (movies: [Movie], totalPages: Int) {
-        let endpoint = MovieEndpoint.search(query: query, page: page)
-        let response: MovieResponseDTO = try await networkService.request(endpoint: endpoint)
-        let movies = response.results.map { $0.toDomain() }
-        return (movies: movies, totalPages: response.totalPages)
+        do {
+            let endpoint = MovieEndpoint.search(query: query, page: page)
+            let response: MovieResponseDTO = try await networkService.request(endpoint: endpoint)
+            let movies = response.results.map { $0.toDomain() }
+            return (movies: movies, totalPages: response.totalPages)
+        } catch {
+            throw mapError(error)
+        }
     }
 
     func fetchMovieDetail(id: Int) async throws -> MovieDetail {
-        let endpoint = MovieEndpoint.detail(id: id)
-        let response: MovieDetailResponseDTO = try await networkService.request(endpoint: endpoint)
-        return response.toDomain()
+        do {
+            let endpoint = MovieEndpoint.detail(id: id)
+            let response: MovieDetailResponseDTO = try await networkService.request(endpoint: endpoint)
+            return response.toDomain()
+        } catch {
+            throw mapError(error)
+        }
+    }
+
+    private func mapError(_ error: Error) -> MovieError {
+        if let movieError = error as? MovieError { return movieError }
+        if error is URLError { return .networkFailure }
+        if error is DecodingError { return .decodingFailure }
+        if case NetworkError.httpError(let statusCode) = error {
+            switch statusCode {
+            case 400..<500: return .invalidRequest
+            case 500..<600: return .serverError
+            default: break
+            }
+        }
+        return .unknown
     }
 }
