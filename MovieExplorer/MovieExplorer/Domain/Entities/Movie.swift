c232b6f1aca2cce73eebed28bdac71c47716bc95
GRJeon <104538652+GRJeon@users.noreply.github.com>
2026-05-03 21:13:37 +0900

    feat: diffable data source 위한 hashable

--- MovieExplorer/MovieExplorer/Domain/Entities/Movie.swift
+++ MovieExplorer/MovieExplorer/Domain/Entities/Movie.swift
@@ -5,7 +5,7 @@
 //  Created by Liam on 4/29/26.
 //
 
-nonisolated struct Movie: Equatable {
+nonisolated struct Movie: Equatable, Hashable {
     let id: Int
     let title: String
     let posterPath: String?
