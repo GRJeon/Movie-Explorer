07833137cf7bec37f2b336920d285d50d82ac244
GRJeon <104538652+GRJeon@users.noreply.github.com>
2026-05-07 11:01:43 +0900

    refactor: Movie의 Equatable 커스텀

--- MovieExplorer/MovieExplorer/Domain/Entities/Movie.swift
+++ MovieExplorer/MovieExplorer/Domain/Entities/Movie.swift
@@ -7,9 +7,27 @@
 
 import Foundation
 
-nonisolated struct Movie: Equatable, Hashable {
+nonisolated struct Movie {
     let id: Int
     let title: String
     let posterPath: URL?
     let voteAverage: Double
+}
+
+extension Movie: Equatable, Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+
+    static func == (lhs: Movie, rhs: Movie) -> Bool {
+        #if DEBUG
+        // 테스트 및 시뮬레이터 환경: 모든 프로퍼티를 꼼꼼하게 비교
+        return lhs.id == rhs.id &&
+               lhs.title == rhs.title &&
+               lhs.voteAverage == rhs.voteAverage
+        #else
+        // rlease: 서버의 데이터가 자주 변하지 않으므로 id만 비교
+        return lhs.id == rhs.id
+        #endif
+    }
 }
